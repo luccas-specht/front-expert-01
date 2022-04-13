26ff21927c77d04fb0afdc49a96f70a5882b6aa0
Luccas Specht <luccasspecht70@gmail.com>
2022-04-12 21:46:46 -0300

    style: add header component

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,7 +1,28 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from './style.module.css';
+
 export function Header() {
   return (
     <header>
-      <h1>Header</h1>
+      <nav className={styles.nav}>
+        <div className={styles.brand}>
+          <Image src="/images/logo.png" />
+        </div>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/favorites">
+              <a>Favorites</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
